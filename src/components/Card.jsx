15d17b27f17f0847048f5ca8a4396e1a2272a9ec
aditya <fordevelopmentusecase@gmail.com>
2025-09-25 22:24:24 +0530

    Enhance routing in App component with ProtectedRoute for authentication; update Card component to accept props for dynamic content; add EcoRank logo to Login and SignUp pages; create new AssignedTaskCard component.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,13 @@
-export default function Card(){
+export default function Card({src, heading, desc, level, ecoPoints, time, }){
   return <div>
-    
+    <img src={src} alt="" />
+    <h1>{heading}</h1>
+    <span>{level}</span>
+    <p>{desc}</p>
+    <div>
+      <span>{ecoPoints}</span>
+      <span>{time}</span>
+    </div>
+    <button>Resume</button>
   </div>
 }
